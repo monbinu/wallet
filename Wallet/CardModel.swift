6b661429a84ab684fcff7f480a407ad49396309d
monbinu <69413509+monbinu@users.noreply.github.com>
2023-11-15 12:18:32 +0100

    fixed } error

--- Wallet/CardModel.swift
+++ Wallet/CardModel.swift
@@ -6,4 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
 
+    //class Card: Identifiable {
+    //var id: ObjectIdentifier
+    
+    //var cardDesign: UUID = UUID()
+    
+    //var image: String = "noimage"
+//}
