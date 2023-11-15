8cb1cd8610ff82ce29f3b72c243fd5f8b421464e
monbinu <69413509+monbinu@users.noreply.github.com>
2023-11-15 12:51:47 +0100

    made viewmodel and model for card designs

---
+++ Wallet/Model/CardModel.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Wallet
+//
+//  Created by Mikayla Kim on 15/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct CardIndividual: Identifiable {
+    
+    var id: UUID = UUID()
+
+    var imageName: String = "noimage"
+    
+    
+}
---
+++ Wallet/ViewModel/CardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardViewModel.swift
+//  Wallet
+//
+//  Created by Mikayla Kim on 15/11/23.
+//
+
+import Foundation
