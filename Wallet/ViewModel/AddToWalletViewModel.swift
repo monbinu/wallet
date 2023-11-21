495cb476095ba3eb13b9b76f34e9ab211b3ee074
monbinu <69413509+monbinu@users.noreply.github.com>
2023-11-21 12:06:00 +0100

    made transaction list, made "add to wallet" function and screen

---
+++ Wallet/ViewModel/AddToWalletViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddToWalletViewModel.swift
+//  Wallet
+//
+//  Created by Mikayla Kim on 21/11/23.
+//
+
+import Foundation
---
+++ Wallet/Views/AddToWalletView.swift
@@ -0,0 +1,18 @@
+//
+//  AddToWalletView.swift
+//  Wallet
+//
+//  Created by Mikayla Kim on 21/11/23.
+//
+
+import SwiftUI
+
+struct AddToWalletView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToWalletView()
+}
